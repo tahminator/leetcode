4ceddf43609a12b0e66bf9971aa592ebef314068
Tahmid Ahmed <tahmid@tahmid.io>
2026-07-01 00:13:08 -0400

    solved 765 more efficiently

--- cpp/leetcode/l0765_couples_holding_hands/Solution.cpp
+++ cpp/leetcode/l0765_couples_holding_hands/Solution.cpp
@@ -11,15 +11,23 @@ public:
       adj[r] = l;
     }
 
+    int curr_pos[row.size()];
+
+    for (size_t i = 0; i < row.size(); i++) {
+      curr_pos[row[i]] = i;
+    }
+
     int swap = 0;
     for (size_t i = 0; i < row.size(); i += 2) {
       int l = row[i], r = row[i + 1];
 
       if (adj[l] != r) {
-        auto curr_r_it = std::find(row.begin(), row.end(), adj[l]);
-        auto curr_r_i = curr_r_it - row.begin();
-        row[curr_r_i] = r;
+        auto curr_index_r = curr_pos[adj[l]];
+        row[curr_index_r] = r;
         row[i + 1] = adj[l];
+
+        curr_pos[r] = curr_index_r;
+        curr_pos[adj[l]] = i + 1;
         swap++;
       }
     }
